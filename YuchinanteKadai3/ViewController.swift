6644fd79b5d25cca56aedaa31cc30ff68a8a3099
akio0911 <akio0911@gmail.com>
2023-10-07 11:40:14 +0900

    feedback

--- YuchinanteKadai3/ViewController.swift
+++ YuchinanteKadai3/ViewController.swift
@@ -10,38 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     // テキストフィールドとUIスイッチ、およびラベルのインターフェース要素を宣言
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
 
-    @IBOutlet weak var switch1: UISwitch!
-    @IBOutlet weak var switch2: UISwitch!
+    @IBOutlet private weak var switch1: UISwitch!
+    @IBOutlet private weak var switch2: UISwitch!
 
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet private weak var label1: UILabel!
+    @IBOutlet private weak var label2: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
 
     // 計算ボタンがタップされたときに実行されるアクション
-    @IBAction func calculateButton(_ sender: Any) {
+    @IBAction private func calculateButton(_ sender: Any) {
         // テキストフィールドの値を数値に変換
-        var number1 = Int(textField1.text ?? "") ?? 0
-        var number2 = Int(textField2.text ?? "") ?? 0
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
 
         // switch1の状態に応じてnumber1の正負を切り替え
+        let signedNumber1: Int
         if switch1.isOn {
-            number1 = -number1
+            signedNumber1 = -number1
+        } else {
+            signedNumber1 = number1
         }
 
         // switch2の状態に応じてnumber2の正負を切り替え
+        let signedNumber2: Int
         if switch2.isOn {
-            number2 = -number2
+            signedNumber2 = -number2
+        } else {
+            signedNumber2 = number2
         }
 
         // 数値の計算: number1とnumber2を加算
-        let result = number1 + number2
+        let result = signedNumber1 + signedNumber2
 
         // ラベルに計算結果を表示
-        label1.text = "\(number1)"
-        label2.text = "\(number2)"
+        label1.text = "\(signedNumber1)"
+        label2.text = "\(signedNumber2)"
         resultLabel.text = "\(result)"
     }
 }
